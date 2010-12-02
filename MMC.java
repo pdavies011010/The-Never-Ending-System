65b9f96029d7b3ec8e4936dee48a9afbbf01ac39
Phill Davies <binary011010@verizon.net>
2010-12-02 15:27:09 -0500

    Continuing work on MMC.

--- MMC.java
+++ MMC.java
@@ -8,6 +8,27 @@ public class MMC {
 	private byte[] cartridgeBankLo = new byte[Constants.CARTRIDGE_BANK_SIZE];
 	private byte[] cartridgeBankHi = new byte[Constants.CARTRIDGE_BANK_SIZE];
 
+	private boolean patternTable0Writable, patternTable1Writable;
+	
+	private byte[] patternTable0 = new byte[Constants.PATTERN_TABLE_SIZE];
+	private byte[] patternTable1 = new byte[Constants.PATTERN_TABLE_SIZE];
+	private byte[] nameTable0 = new byte[Constants.NAME_TABLE_SIZE];
+	private byte[] attributeTable0 = new byte[Constants.ATTRIBUTE_TABLE_SIZE];
+	private byte[] nameTable1 = new byte[Constants.NAME_TABLE_SIZE];
+	private byte[] attributeTable1 = new byte[Constants.ATTRIBUTE_TABLE_SIZE];
+	private byte[] nameTable2 = new byte[Constants.NAME_TABLE_SIZE];
+	private byte[] attributeTable2 = new byte[Constants.ATTRIBUTE_TABLE_SIZE];
+	private byte[] nameTable3 = new byte[Constants.NAME_TABLE_SIZE];
+	private byte[] attributeTable3 = new byte[Constants.ATTRIBUTE_TABLE_SIZE];
+	private byte[] imagePalette = new byte[Constants.PALETTE_SIZE];
+	private byte[] spritePalette = new byte[Constants.PALETTE_SIZE];
+	private byte[] spriteMem = new byte[Constants.SPRITE_MEM_SIZE];
+	
+	public MMC() {
+		patternTable0Writable = false;
+		patternTable1Writable = false;
+	}
+	
 	public byte[] getCpuRam() {
 		return cpuRam;
 	}
@@ -22,5 +43,57 @@ public class MMC {
 	
 	public byte[] getCartridgeBankHi() {
 		return cartridgeBankHi;
+	}
+	
+	public byte[] getPatternTable0() {
+		return patternTable0;
+	}
+	
+	public byte[] getPatternTable1() {
+		return patternTable1;
+	}
+	
+	public byte[] getNameTable0() {
+		return nameTable0;
+	}
+	
+	public byte[] getAttributeTable0() {
+		return attributeTable0;
+	}
+	
+	public byte[] getNameTable1() {
+		return nameTable1;
+	}
+	
+	public byte[] getAttributeTable1() {
+		return attributeTable1;
+	}
+	
+	public byte[] getNameTable2() {
+		return nameTable2;
+	}
+	
+	public byte[] getAttributeTable2() {
+		return attributeTable2;
+	}
+	
+	public byte[] getNameTable3() {
+		return nameTable3;
+	}
+	
+	public byte[] getAttributeTable3() {
+		return attributeTable3;
+	}
+	
+	public byte[] getImagePalette() {
+		return imagePalette;
+	}
+	
+	public byte[] getSpritePalette() {
+		return spritePalette;
+	}
+	
+	public byte[] getSpriteMem() {
+		return spriteMem;
 	}
 }
